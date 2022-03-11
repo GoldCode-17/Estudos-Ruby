6df82889006d6162a12b5ca66b0fda6107081592
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-03-10 21:38:51 -0300

    funcionando

--- Algoritmos/binary_search.rb
+++ Algoritmos/binary_search.rb
@@ -1,10 +1,22 @@
 def binary_search(lista, item, comeco, fim)
-  meio = lista.count / 2
-  if item == meio
-
+  if comeco <= fim
+    meio = (comeco + fim) / 2
+    if lista[meio] == item
+      puts meio
+    else
+      if item > meio
+        binary_search(lista, item, meio + 1, fim)
+      else
+        binary_search(lista, item, comeco, fim - 1)
+      end
+    end
+  else
+    puts -1
   end
 end
 
 arr = [2, 3, 4, 10, 40]
-valor = 10
+valor = 40
+
+binary_search(arr, valor, 0, 4)
 
