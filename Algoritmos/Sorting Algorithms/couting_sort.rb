17ed5ba3f742b24ca29f1aa97571317847b6f78f
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-03-15 23:04:44 -0300

    final

--- Algoritmos/Sorting Algorithms/couting_sort.rb
+++ Algoritmos/Sorting Algorithms/couting_sort.rb
@@ -1,23 +1,21 @@
 def couting_sort(entrada)
-  #definindo o maior elemento da lista
+  #identificando o maior elemento da lista
   n = entrada.max
 
-  #criando a lista de contagem a quantidade de números repetidos
+  #criando a lista de contagem e identificando a quantidade de números repetidos
   contador = Array.new(n + 1) { |n| 0 }
   entrada.each do |i|
     contador[i] += 1
   end
 
   #somando cada elemento do contador com o anterior para achar a localização dentro do array de saída
-  n = contador.count
-  contador.each_index do |i|
-    contador[i] = contador[i] + contador[i - 1] if i > 0
+  (1..contador.count - 1).each do |i|
+    contador[i] = contador[i] + contador[i - 1]
   end
 
   #ordenando os elementos na lista de saída a partir da lista contador
-  saida = Array.new(10)
-  qtd = entrada.count
-  qtd -= 1
+  saida = Array.new(entrada.count)
+  qtd = entrada.count - 1
   qtd.downto(0) do |i|
     j = contador[entrada[i]]
     saida[j - 1] = entrada[i]
@@ -27,5 +25,5 @@ def couting_sort(entrada)
   return saida
 end
 
-array = [2, 5, 3, 0, 2, 3, 0, 5, 3, 0]
+array = [2, 7, 3, 8, 9, 1, 5]
 print "A matriz de elementos ordenada é:\n#{couting_sort(array)}"
